dd5a3afb9ad916c8461927af6ceac6a7ee7b2c01
htoomyataung <htoomyatag.webdev@gmail.com>
2015-12-20 20:50:08 +0630

    product create by seller work

--- app/controllers/sellers/sessions_controller.rb
+++ app/controllers/sellers/sessions_controller.rb
@@ -2,11 +2,14 @@ class Sellers::SessionsController < Devise::SessionsController
   
 
    	def after_sign_in_path_for(sellers)
+
+   		 @raw_store_id = Store.where(:seller_id => current_seller.id).pluck(:id)
+     	 @store_id = @raw_store_id.to_s.gsub("[", "").gsub("]", "")
    		
    	  if current_seller.has_store == nil
           return new_store_path
       else
-          return stores_path	
+          return "/stores/#{@store_id}"	
       end
 
   	end
