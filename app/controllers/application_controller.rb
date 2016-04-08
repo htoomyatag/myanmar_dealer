3ed95f27d645d0a4169a4435e5823956fd8e05d1
htoomyataung <htoomyatag.webdev@gmail.com>
2016-04-08 16:15:00 +0630

    local added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   skip_before_filter :verify_authenticity_token  
+  before_action :set_locale
   
   before_filter :configure_permitted_parameters, if: :devise_controller?
+
+def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 
 
 
--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,10 @@ module MyanmarDealer
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.i18n.enforce_available_locales = false
+    config.i18n.available_locales = [:en, :mm]
+    config.i18n.default_locale = :en
+
   end
 end
