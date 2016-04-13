b073b7efcd914543a5caf282dc8b8ddde34bb60a
htoomyataung <htoomyatag.webdev@gmail.com>
2016-04-13 16:39:22 +0630

    chat work

--- app/controllers/fronts_controller.rb
+++ app/controllers/fronts_controller.rb
@@ -79,117 +79,186 @@ class FrontsController < ApplicationController
     def full_productinfo_bath_supplies
       @product = Product.find(params[:id])
       @relate_products = Product.where(:category => "bath_supplies")
+
+      @users = User.where.not("id = ?",current_user.id).order("created_at DESC").where("id = ?", @product.user_id)
+      @conversations = Conversation.involving(current_user).order("created_at DESC")
     end
 
     def full_productinfo_beauty_equipments
       @product = Product.find(params[:id])
       @relate_products = Product.where(:category => "beauty_equipments")
+
+      @users = User.where.not("id = ?",current_user.id).order("created_at DESC").where("id = ?", @product.user_id)
+      @conversations = Conversation.involving(current_user).order("created_at DESC")
     end
 
     def full_productinfo_car_accessories
       @product = Product.find(params[:id])
       @relate_products = Product.where(:category => "car_accessories")
+
+      @users = User.where.not("id = ?",current_user.id).order("created_at DESC").where("id = ?", @product.user_id)
+      @conversations = Conversation.involving(current_user).order("created_at DESC")
     end
 
     def full_productinfo_computers
        @product = Product.find(params[:id])
        @relate_products = Product.where(:category => "computers_laptops")
+
+       @users = User.where.not("id = ?",current_user.id).order("created_at DESC").where("id = ?", @product.user_id)
+       @conversations = Conversation.involving(current_user).order("created_at DESC")
     end
 
     def full_productinfo_electrical_equipments
        @product = Product.find(params[:id])
        @relate_products = Product.where(:category => "eletronic_related")
+
+       @users = User.where.not("id = ?",current_user.id).order("created_at DESC").where("id = ?", @product.user_id)
+       @conversations = Conversation.involving(current_user).order("created_at DESC")
     end
 
     def full_productinfo_equipments
         @product = Product.find(params[:id])
         @relate_products = Product.where(:category => "equipments")
+
+        @users = User.where.not("id = ?",current_user.id).order("created_at DESC").where("id = ?", @product.user_id)
+        @conversations = Conversation.involving(current_user).order("created_at DESC")
     end
 
     def full_productinfo_fashion_accessories
         @product = Product.find(params[:id])
         @relate_products = Product.where(:category => "fashion_related")
+
+        @users = User.where.not("id = ?",current_user.id).order("created_at DESC").where("id = ?", @product.user_id)
+        @conversations = Conversation.involving(current_user).order("created_at DESC")
     end
 
     def full_productinfo_fashion_bags
         @product = Product.find(params[:id])
         @relate_products = Product.where(:category => "bags")
+
+        @users = User.where.not("id = ?",current_user.id).order("created_at DESC").where("id = ?", @product.user_id)
+        @conversations = Conversation.involving(current_user).order("created_at DESC")
     end
 
     def full_productinfo_fashion_clothings
       @product = Product.find(params[:id])
       @relate_products = Product.where(:category => "cloths")
+
+      @users = User.where.not("id = ?",current_user.id).order("created_at DESC").where("id = ?", @product.user_id)
+      @conversations = Conversation.involving(current_user).order("created_at DESC")
     end
 
     def full_productinfo_fashion_footwears
         @product = Product.find(params[:id])
         @relate_products = Product.where(:category => "footwears")
+
+        @users = User.where.not("id = ?",current_user.id).order("created_at DESC").where("id = ?", @product.user_id)
+        @conversations = Conversation.involving(current_user).order("created_at DESC")
     end
 
 
     def full_productinfo_fashion_hats
         @product = Product.find(params[:id])
         @relate_products = Product.where(:category => "hats")
+
+        @users = User.where.not("id = ?",current_user.id).order("created_at DESC").where("id = ?", @product.user_id)
+        @conversations = Conversation.involving(current_user).order("created_at DESC")
     end
 
     def full_productinfo_gifts
         @product = Product.find(params[:id])
         @relate_products = Product.where(:category => "gifts")
+
+        @users = User.where.not("id = ?",current_user.id).order("created_at DESC").where("id = ?", @product.user_id)
+        @conversations = Conversation.involving(current_user).order("created_at DESC")
     end
 
     def full_productinfo_home_appliances
         @product = Product.find(params[:id])
         @relate_products = Product.where(:category => "home_appliance")
+
+        @users = User.where.not("id = ?",current_user.id).order("created_at DESC").where("id = ?", @product.user_id)
+       @conversations = Conversation.involving(current_user).order("created_at DESC")
     end
 
     def full_productinfo_instruments
         @product = Product.find(params[:id])
         @relate_products = Product.where(:category => "instruments")
+
+        @users = User.where.not("id = ?",current_user.id).order("created_at DESC").where("id = ?", @product.user_id)
+        @conversations = Conversation.involving(current_user).order("created_at DESC")
     end
 
     def full_productinfo_machines
          @product = Product.find(params[:id])
         @relate_products = Product.where(:category => "machines")
+
+        @users = User.where.not("id = ?",current_user.id).order("created_at DESC").where("id = ?", @product.user_id)
+        @conversations = Conversation.involving(current_user).order("created_at DESC")
     end
 
     def full_productinfo_makeup_and_skincares
         @product = Product.find(params[:id])
         @relate_products = Product.where(:category => "comesmetics")
+
+        @users = User.where.not("id = ?",current_user.id).order("created_at DESC").where("id = ?", @product.user_id)
+        @conversations = Conversation.involving(current_user).order("created_at DESC")
     end
 
     def full_productinfo_medicines
         @product = Product.find(params[:id])
         @relate_products = Product.where(:category => "medicines")
+
+        @users = User.where.not("id = ?",current_user.id).order("created_at DESC").where("id = ?", @product.user_id)
+        @conversations = Conversation.involving(current_user).order("created_at DESC")
     end
 
     def full_productinfo_motorcycle_accessories
         @product = Product.find(params[:id])
         @relate_products = Product.where(:category => "motorcycle")
+
+        @users = User.where.not("id = ?",current_user.id).order("created_at DESC").where("id = ?", @product.user_id)
+        @conversations = Conversation.involving(current_user).order("created_at DESC")
     end
 
     def full_productinfo_services
         @product = Product.find(params[:id])
         @relate_products = Product.where(:category => "service_category")
+
+        @users = User.where.not("id = ?",current_user.id).order("created_at DESC").where("id = ?", @product.user_id)
+        @conversations = Conversation.involving(current_user).order("created_at DESC")
     end
 
     def full_productinfo_sports
        @product = Product.find(params[:id])
        @relate_products = Product.where(:category => "sports")
+
+       @users = User.where.not("id = ?",current_user.id).order("created_at DESC").where("id = ?", @product.user_id)
+       @conversations = Conversation.involving(current_user).order("created_at DESC")
     end
 
     def full_productinfo_telephone_accessories
        @product = Product.find(params[:id])
        @relate_products = Product.where(:category => "phone_related")
+
+       @users = User.where.not("id = ?",current_user.id).order("created_at DESC").where("id = ?", @product.user_id)
+       @conversations = Conversation.involving(current_user).order("created_at DESC")
     end
 
     def full_productinfo_toys
        @product = Product.find(params[:id])
        @relate_products = Product.where(:category => "toys")
+
+      @users = User.where.not("id = ?",current_user.id).order("created_at DESC").where("id = ?", @product.user_id)
+       @conversations = Conversation.involving(current_user).order("created_at DESC")
     end
 
     def full_productinfo_training_and_schools
        @product = TrainingAndSchool.find(params[:id])
        @relate_products = TrainingAndSchool.all
+
+        @users = User.where.not("id = ?",current_user.id).order("created_at DESC").where("id = ?", @product.user_id)
+       @conversations = Conversation.involving(current_user).order("created_at DESC")
     end
 
     def register
@@ -201,6 +270,7 @@ class FrontsController < ApplicationController
         @fashion_hats = Product.where(:category => "hats")
         @fashion_footwears = Product.where(:category => "footwears")
         @fashion_clothings = Product.where(:category => "cloths")
+
     end
      
     def electronics_related
@@ -208,6 +278,8 @@ class FrontsController < ApplicationController
         @computers = Product.where(:category => "computers_laptops")
         @telephone_accessories = Product.where(:category => "phone_related")
         @home_appliances = Product.where(:category => "home_appliance")
+
+     
     end
 
     def health_beauty_related
@@ -215,6 +287,8 @@ class FrontsController < ApplicationController
        @bath_supplies = Product.where(:category => "bath_supplies")
        @beauty_equipments = Product.where(:category => "beauty_equipments")
        @makeup_and_skincares = Product.where(:category => "comesmetics")
+
+
      
     end
      
@@ -223,22 +297,30 @@ class FrontsController < ApplicationController
        @product_machines = Product.where(:category => "machines")
        @instruments = Product.where(:category => "instruments")
 
+   
+
 
     end
     def auto_transportation
        @car_accessories = Product.where(:category => "car_accessories")
        @motorcycle_accessories = Product.where(:category => "motorcycle")
+
+
     end
     def sports_related
        @sports = Product.where(:category => "sports")
        @gifts = Product.where(:category => "gifts")
        @toys = Product.where(:category => "toys")
 
+
+
     end 
 
     def service_related
         @customer_services = Service.all
         @training_and_schools = TrainingAndSchool.all
+
+    
     end
 
 
