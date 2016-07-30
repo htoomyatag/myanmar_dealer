8517c6eadfa9fa85573fafd71c4dab8d3129011f
htoomyataung <htoomyatag.webdev@gmail.com>
2016-07-30 14:17:36 +0630

    desc added

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -65,7 +65,7 @@ class ApisController < ApplicationController
   def my_api_sports
 
     @sports = Product.where(:category => "sports").select(:id,:title,:price,:store_name, :price, :quantity, :avatar1_file_name, :avatar2_file_name, :avatar3_file_name, :avatar4_file_name)
-    @specifications = Product.select(:id,:weight, :made_by_country, :made_with, :color, :brand).where(:category => "sports")
+    @specifications = Product.select(:id,:description,:weight, :made_by_country, :made_with, :color, :brand).where(:category => "sports")
 
     respond_to do |format|
           my_primary_json = { :Products => @sports.to_json(:methods => [:avatar_url]), :Specifications => @specifications.to_json   }
@@ -81,7 +81,7 @@ class ApisController < ApplicationController
   
   def my_api_toys
     @toys = Product.where(:category => "toys").select(:id,:title,:price,:store_name, :price, :quantity, :avatar1_file_name, :avatar2_file_name, :avatar3_file_name, :avatar4_file_name)
-    @specifications = Product.select(:id,:weight,:made_by_country,:made_with,:color,:age_group).where(:category => "toys")
+    @specifications = Product.select(:id,:description,:weight,:made_by_country,:made_with,:color,:age_group).where(:category => "toys")
     
   
     respond_to do |format|
@@ -103,7 +103,7 @@ class ApisController < ApplicationController
   
   def my_api_gifts
     @gifts = Product.where(:category => "gifts").select(:id,:title,:price,:store_name, :price, :quantity, :avatar1_file_name, :avatar2_file_name, :avatar3_file_name, :avatar4_file_name)
-    @specifications = Product.select(:id,:weight, :made_by_country, :made_with, :description, :color).where(:category => "gifts")
+    @specifications = Product.select(:id,:description,:weight, :made_by_country, :made_with, :description, :color).where(:category => "gifts")
 
 
     respond_to do |format|
@@ -120,7 +120,7 @@ class ApisController < ApplicationController
   
   def my_api_car_accessories
     @car_accessories = Product.where(:category => "car_accessories").select(:id,:title,:price,:store_name, :price, :quantity, :avatar1_file_name, :avatar2_file_name, :avatar3_file_name, :avatar4_file_name)
-    @specifications = Product.select(:id,:size, :weight, :made_by_country, :made_with, :brand, :specification, :modal_number, :color, :feature).where(:category => "car_accessories")
+    @specifications = Product.select(:id,:description,:size, :weight, :made_by_country, :made_with, :brand, :specification, :modal_number, :color, :feature).where(:category => "car_accessories")
 
 
     respond_to do |format|
@@ -158,7 +158,7 @@ class ApisController < ApplicationController
   def my_api_machines
 
     @machines = Product.where(:category => "machines").select(:id,:title,:price,:store_name, :price, :quantity, :avatar1_file_name, :avatar2_file_name, :avatar3_file_name, :avatar4_file_name)
-    @specifications = Product.select(:id,:dimension, :weight, :made_by_country, :made_with, :description, :brand, :category, :modal_number, :specification, :color).where(:category => "machines")
+    @specifications = Product.select(:id,:description,:dimension, :weight, :made_by_country, :made_with, :description, :brand, :category, :modal_number, :specification, :color).where(:category => "machines")
 
     respond_to do |format|
           my_primary_json = { :Products => @machines.to_json(:methods => [:avatar_url]), :Specifications => @specifications.to_json   }
@@ -177,7 +177,7 @@ class ApisController < ApplicationController
   def my_api_instruments
 
     @instruments = Product.where(:category => "instruments").select(:id,:title,:price,:store_name, :price, :quantity, :avatar1_file_name, :avatar2_file_name, :avatar3_file_name, :avatar4_file_name)
-    @specifications = Product.select(:id,:dimension, :weight, :made_by_country, :made_with, :description, :brand, :modal_number, :specification, :color).where(:category => "instruments")
+    @specifications = Product.select(:id,:description,:dimension, :weight, :made_by_country, :made_with, :description, :brand, :modal_number, :specification, :color).where(:category => "instruments")
 
 
     respond_to do |format|
@@ -198,7 +198,7 @@ class ApisController < ApplicationController
   def my_api_equipment
 
     @equipments = Product.where(:category => "equipments").select(:id,:title,:price,:store_name, :price, :quantity, :avatar1_file_name, :avatar2_file_name, :avatar3_file_name, :avatar4_file_name)
-  @specifications = Product.select(:id, :size, :weight, :made_by_country, :made_with, :description, :brand, :specification, :modal_number, :color).where(:category => "equipments")
+  @specifications = Product.select(:id,:description, :size, :weight, :made_by_country, :made_with, :description, :brand, :specification, :modal_number, :color).where(:category => "equipments")
 
     respond_to do |format|
           my_primary_json = { :Products => @equipments.to_json(:methods => [:avatar_url]), :Specifications => @specifications.to_json   }
@@ -216,7 +216,7 @@ class ApisController < ApplicationController
   def my_api_bath_supplies
 
     @bath_supplies = Product.where(:category => "bath_supplies").select(:id,:title,:price,:store_name, :price, :quantity, :avatar1_file_name, :avatar2_file_name, :avatar3_file_name, :avatar4_file_name)
-    @specifications = Product.select(:id,:ingredient, :usage, :made_by_country, :description, :brand, :effect, :certification, :age_group).where(:category => "bath_supplies")
+    @specifications = Product.select(:id,:description,:ingredient, :usage, :made_by_country, :description, :brand, :effect, :certification, :age_group).where(:category => "bath_supplies")
 
 
     respond_to do |format|
@@ -255,7 +255,7 @@ class ApisController < ApplicationController
   def my_api_beauty_equipments
 
     @beauty_equipments = Product.where(:category => "beauty_equipments").select(:id,:title,:price,:store_name, :price, :quantity, :avatar1_file_name, :avatar2_file_name, :avatar3_file_name, :avatar4_file_name)
-    @specifications = Product.select(:id,:brand, :made_by_country, :feature, :modal_number, :color).where(:category => "beauty_equipments")
+    @specifications = Product.select(:id,:description,:brand, :made_by_country, :feature, :modal_number, :color).where(:category => "beauty_equipments")
 
     respond_to do |format|
           my_primary_json = { :Products => @beauty_equipments.to_json(:methods => [:avatar_url]), :Specifications => @specifications.to_json   }
@@ -273,7 +273,7 @@ class ApisController < ApplicationController
   def my_api_medicines
 
     @medicines = Product.where(:category => "medicines").select(:id,:title,:price,:store_name, :price, :quantity, :avatar1_file_name, :avatar2_file_name, :avatar3_file_name, :avatar4_file_name)
-    @specifications = Product.select(:id,:certification, :ingredient, :usage, :made_by_country, :caution).where(:category => "medicines")
+    @specifications = Product.select(:id,:description,:certification, :ingredient, :usage, :made_by_country, :caution).where(:category => "medicines")
 
     respond_to do |format|
           my_primary_json = { :Products => @medicines.to_json(:methods => [:avatar_url]), :Specifications => @specifications.to_json   }
@@ -292,7 +292,7 @@ class ApisController < ApplicationController
   def my_api_home_appliances
 
     @home_appliances = Product.where(:category => "home_appliance").select(:id,:title,:price,:store_name, :price, :quantity, :avatar1_file_name, :avatar2_file_name, :avatar3_file_name, :avatar4_file_name)
-      @specifications = Product.select(:id,:modal_number, :color, :feature, :power, :voltage,:size, :weight, :made_by_country).where(:category => "home_appliance")
+      @specifications = Product.select(:id,:description,:modal_number, :color, :feature, :power, :voltage,:size, :weight, :made_by_country).where(:category => "home_appliance")
 
     respond_to do |format|
           my_primary_json = { :Products => @home_appliances.to_json(:methods => [:avatar_url]), :Specifications => @specifications.to_json   }
@@ -310,7 +310,7 @@ class ApisController < ApplicationController
   def my_api_computers
 
     @computers = Product.where(:category => "computers_laptops").select(:id,:title,:price,:store_name, :price, :quantity, :avatar1_file_name, :avatar2_file_name, :avatar3_file_name, :avatar4_file_name)
-   @specifications = Product.select(:id,:size,:weight,:thickness,:made_by_country,:brand,:color,:feature,:modal_number,:operation_system ).where(:category => "computers_laptops")
+   @specifications = Product.select(:id,:description,:size,:weight,:thickness,:made_by_country,:brand,:color,:feature,:modal_number,:operation_system ).where(:category => "computers_laptops")
 
     respond_to do |format|
           my_primary_json = { :Products => @computers.to_json(:methods => [:avatar_url]), :Specifications => @specifications.to_json   }
@@ -329,7 +329,7 @@ class ApisController < ApplicationController
   def my_api_telephone_accessories
 
     @telephone_accessories = Product.where(:category => "phone_related").select(:id,:title,:price,:store_name, :price, :quantity, :avatar1_file_name, :avatar2_file_name, :avatar3_file_name, :avatar4_file_name)
-   @specifications = Product.select(:id,:size,:weight,:feature,:color,:modal_number,:brand).where(:category => "phone_related")
+   @specifications = Product.select(:id,:description,:size,:weight,:feature,:color,:modal_number,:brand).where(:category => "phone_related")
 
     respond_to do |format|
           my_primary_json = { :Products => @telephone_accessories.to_json(:methods => [:avatar_url]), :Specifications => @specifications.to_json   }
@@ -366,7 +366,7 @@ class ApisController < ApplicationController
   def my_api_fashion_hats
 
     @fashion_hats = Product.where(:category => "hats").select(:id,:title,:price,:store_name, :price, :quantity, :avatar1_file_name, :avatar2_file_name, :avatar3_file_name, :avatar4_file_name)
-      @specifications = Product.select(:id,:made_by_country,:made_with,:hat_type,:brand).where(:category => "hats")
+      @specifications = Product.select(:id,:description,:made_by_country,:made_with,:hat_type,:brand).where(:category => "hats")
 
     respond_to do |format|
           my_primary_json = { :Products => @fashion_hats.to_json(:methods => [:avatar_url]), :Specifications => @specifications.to_json   }
@@ -384,7 +384,7 @@ class ApisController < ApplicationController
   def my_api_fashion_footwears
 
     @fashion_footwears = Product.where(:category => "footwears").select(:id,:title,:price,:store_name, :price, :quantity, :avatar1_file_name, :avatar2_file_name, :avatar3_file_name, :avatar4_file_name)
-    @specifications = Product.select(:id,:made_by_country,:made_with,:footwear_type,:size,:brand,:color  ).where(:category => "footwears")
+    @specifications = Product.select(:id,:description,:made_by_country,:made_with,:footwear_type,:size,:brand,:color  ).where(:category => "footwears")
 
     respond_to do |format|
           my_primary_json = { :Products => @fashion_footwears.to_json(:methods => [:avatar_url]), :Specifications => @specifications.to_json   }
@@ -402,7 +402,7 @@ class ApisController < ApplicationController
   def my_api_fashion_bags
 
     @fashion_bags = Product.where(:category => "bags").select(:id,:title,:price,:store_name, :price, :quantity, :avatar1_file_name, :avatar2_file_name, :avatar3_file_name, :avatar4_file_name)
-     @specifications = Product.select(:id,:made_by_country,:made_with,:bag_type,:size,:brand,:color ).where(:category => "bags")
+     @specifications = Product.select(:id,:description,:made_by_country,:made_with,:bag_type,:size,:brand,:color ).where(:category => "bags")
 
 
     respond_to do |format|
@@ -421,7 +421,7 @@ class ApisController < ApplicationController
   def my_api_fashion_accessories
 
     @fashion_accessories = Product.where(:category => "fashion_related").select(:id,:title,:price,:store_name, :price, :quantity, :avatar1_file_name, :avatar2_file_name, :avatar3_file_name, :avatar4_file_name)
-       @specifications = Product.select(:id,:made_by_country,:made_with,:brand).where(:category => "fashion_related")
+       @specifications = Product.select(:id,:description,:made_by_country,:made_with,:brand).where(:category => "fashion_related")
 
     respond_to do |format|
           my_primary_json = { :Products => @fashion_accessories.to_json(:methods => [:avatar_url]), :Specifications => @specifications.to_json   }
@@ -441,7 +441,7 @@ class ApisController < ApplicationController
 
 
     @fashion_clothings = Product.where(:category => "cloths").select(:id,:title,:price,:store_name, :price, :quantity, :avatar1_file_name, :avatar2_file_name, :avatar3_file_name, :avatar4_file_name)
-    @specifications = Product.select(:id,:size,:made_by_country,:made_with,:gender,:brand,:color).where(:category => "cloths")
+    @specifications = Product.select(:id,:description,:size,:made_by_country,:made_with,:gender,:brand,:color).where(:category => "cloths")
 
     respond_to do |format|
           my_primary_json = { :Products => @fashion_clothings.to_json(:methods => [:avatar_url]), :Specifications => @specifications.to_json }
