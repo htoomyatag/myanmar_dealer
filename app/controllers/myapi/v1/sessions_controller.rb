1583ad69ba482ba4020d2344ddc594f4603daf6e
htoomyataung <htoomyatag.webdev@gmail.com>
2016-08-04 09:57:13 +0630

    return fields in users

--- app/controllers/myapi/v1/sessions_controller.rb
+++ app/controllers/myapi/v1/sessions_controller.rb
@@ -14,7 +14,7 @@ class Myapi::V1::SessionsController < Devise::SessionsController
     render :status => 200,
            :json => { :success => true,
                       :info => "Logged in",
-                      :data => { :auth_token => current_user.authentication_token } }
+                      :data => { :auth_token => current_user.authentication_token }, :user_id => current_user.id, :user_name => current_user.name, :email => current_user.email}
 
 
     require "base64"
